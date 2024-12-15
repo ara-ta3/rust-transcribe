91327b53d9faef078eb99be36713b62ef9a109d3
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-15 20:48:55 +0900

    Add sample

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,73 @@
 use serde::Serialize;
+use std::{fs::File, io::Write};
 
-#[derive(Serialize)]
+type GenericError = Box<dyn std::error::Error>;
+type GenericResult<T> = Result<T, GenericError>;
+
+#[derive(Serialize, Clone)]
 enum TaskStatus {
     Todo,
     Doing,
     Done,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Clone)]
 struct Task {
     status: TaskStatus,
 }
 
-fn main() -> Result<(), std::io::Error> {
+impl Task {
+    fn updated(&self, _: TaskStatus) -> GenericResult<&Self> {
+        Ok(self)
+    }
+}
+
+trait TaskRepository {
+    fn put(&self, t: Task) -> GenericResult<()>;
+}
+
+struct TaskService<R: TaskRepository> {
+    task_repository: R,
+}
+
+impl <R: TaskRepository> TaskService<R> {
+    pub fn new(r: R) -> Self {
+        Self {task_repository : r}
+    }
+
+    pub fn update(&self, t: Task, next_status: TaskStatus) -> GenericResult<Task> {
+        let n = t.updated(next_status)?;
+        self.task_repository.put(n.clone())?;
+        Ok(n.clone())
+    }
+}
+
+struct TaskRepositoryJson {
+    path: String,
+}
+
+impl TaskRepository for TaskRepositoryJson {
+    fn put(&self, t: Task) -> GenericResult<()> {
+        let json = serde_json::to_string(&t)?;
+        let mut f = File::create(&self.path)?;
+        f.write_all(json.as_bytes())?;
+        Ok(())
+    }
+}
+
+
+fn main()  {
+    let s = TaskService::new(
+        TaskRepositoryJson {
+            path: "./task.json".to_string(),
+        }
+    );
     let t = Task {
         status: TaskStatus::Done
     };
-    let json = serde_json::to_string(&t)?;
-    println!("{}", json);
-    Ok(())
+
+    match s.update(t, TaskStatus::Doing) {
+        Ok(_) => println!("ok"),
+        Err(e) => eprintln!("{:?}", e),
+    }
 }
