bdecb8477ed9de640d198eb8070540da8c8d3874
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-15 21:09:32 +0900

    unimplemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serde::Serialize;
-use std::{fs::File, io::Write};
+use std::{fs::File, io::Write, os::unix::raw::uid_t};
 
 type GenericError = Box<dyn std::error::Error>;
 type GenericResult<T> = Result<T, GenericError>;
@@ -11,14 +11,25 @@ enum TaskStatus {
     Done,
 }
 
+struct User {
+    tasks: [Task],
+}
+
+impl User {
+    fn update(&self, next_status: TaskStatus) -> GenericResult<&[Task]> {
+        unimplemented!();
+    }
+}
+
 
 impl TaskStatus {
-    // fn updated(&self, next: TaskStatus) -> GenericResult<Self> {
-    //     match (self, next) {
-    //         (TaskStatus::Todo, _) => Ok(next),
-    //         (TaskStatus::Doing, TaskStatus::Doing) => Err(std::error::Error )
-    //     }
-    // }
+    fn updated(&self, next: TaskStatus) -> GenericResult<Self> {
+        unimplemented!();
+        // match (self, next) {
+        //     (TaskStatus::Todo, _) => Ok(next),
+        //     (TaskStatus::Doing, TaskStatus::Doing) => Err(std::error::Error )
+        // }
+    }
 }
 
 #[derive(Serialize, Clone)]
@@ -33,6 +44,8 @@ impl Task {
 }
 
 trait TaskRepository {
+    fn fetch(&self, user_id: i32) -> GenericResult<&User>;
+
     fn put(&self, t: Task) -> GenericResult<()>;
 }
 
@@ -45,10 +58,14 @@ impl <R: TaskRepository> TaskService<R> {
         Self {task_repository : r}
     }
 
-    pub fn update(&self, t: Task, next_status: TaskStatus) -> GenericResult<Task> {
-        let n = t.updated(next_status)?;
-        self.task_repository.put(n.clone())?;
-        Ok(n.clone())
+    pub fn update(&self, user_id: i32, next_status: TaskStatus) -> GenericResult<()> {
+        let u = self.task_repository.fetch(user_id)?;
+        let ns = u.update(next_status)?;
+        
+        for n in ns {
+            self.task_repository.put(n.clone())?;
+        }
+        Ok(())
     }
 }
 
@@ -57,6 +74,10 @@ struct TaskRepositoryJson {
 }
 
 impl TaskRepository for TaskRepositoryJson {
+    fn fetch(&self, user_id: i32) -> GenericResult<&User> {
+        unimplemented!()
+    }
+
     fn put(&self, t: Task) -> GenericResult<()> {
         let json = serde_json::to_string(&t)?;
         let mut f = File::create(&self.path)?;
@@ -72,11 +93,7 @@ fn main()  {
             path: "./task.json".to_string(),
         }
     );
-    let t = Task {
-        status: TaskStatus::Done
-    };
-
-    match s.update(t, TaskStatus::Doing) {
+    match s.update(1, TaskStatus::Doing) {
         Ok(_) => println!("ok"),
         Err(e) => eprintln!("{:?}", e),
     }
