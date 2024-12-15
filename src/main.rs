855fa4a0fbc99b46a27cba9c664af14841576dca
Arata Tanaka <2089153+ara-ta3@users.noreply.github.com>
2024-12-15 20:25:55 +0900

    Add Add sample

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Serialize;
+
+#[derive(Serialize)]
+enum TaskStatus {
+    Todo,
+    Doing,
+    Done,
+}
+
+#[derive(Serialize)]
+struct Task {
+    status: TaskStatus,
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let t = Task {
+        status: TaskStatus::Done
+    };
+    let json = serde_json::to_string(&t)?;
+    println!("{}", json);
+    Ok(())
 }
